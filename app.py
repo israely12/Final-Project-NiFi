9ae067f7ae981e18df353b5f5f801721bc812d4f
israel <User1@ULIK-DESKTOP1>
2025-08-05 16:57:07 +0300

    function that gets the full process group object by id created successfuly

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.nifi_modules.connect_to_nifi import NiFiClient
 from src.nifi_modules.create_funnel import create_funnel
 from src.nifi_modules.create_process_group import create_process_group
+from src.nifi_modules.get_process_group_by_id import get_process_group_by_id
 import os
 from dotenv import load_dotenv
 
@@ -15,6 +16,7 @@ def main():
     client.connect_and_print_about()
     create_funnel(client)
     create_process_group(client)
+    get_process_group_by_id(client)
 
 if __name__ == "__main__":
     main()
