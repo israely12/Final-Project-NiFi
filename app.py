3f51691e493a788dc6d2bbff54369029009f0e7c
israel <User1@ULIK-DESKTOP1>
2025-08-10 14:26:55 +0300

    update module function and api route created successfully

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.nifi_modules.connect_to_nifi import NiFiClient
 from src.nifi_modules.create_funnel import create_funnel
 from src.nifi_modules.create_process_group import create_process_group
 from src.nifi_modules.get_process_group_by_id import get_process_group_by_id
+from src.nifi_modules.update_process_group_name import update_process_group_name
 from src.rest_api.run_flask import create_app
 import os
 from dotenv import load_dotenv
@@ -9,6 +10,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MODE = os.getenv("RUN_MODE")
+
+new_process_group_name = os.getenv("UPDATE_PROCESS_GROUP")
 def run_modules():
     client = NiFiClient(
         base_url=os.getenv("NIFI_BASE_URL"),
@@ -19,6 +22,7 @@ def run_modules():
     create_funnel(client)
     create_process_group(client)
     get_process_group_by_id(client)
+    update_process_group_name(client,new_process_group_name)
 
 def run_api():
     app = create_app()
--- src/rest_api/routes/process_group_routes.py
+++ src/rest_api/routes/process_group_routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from src.rest_api.client_provider import client
 from src.nifi_modules.create_process_group import create_process_group
 from src.nifi_modules.get_process_group_by_id import get_process_group_by_id
+from src.nifi_modules.update_process_group_name import update_process_group_name
+
 
 process_group_bp = Blueprint("process_group", __name__)
 
@@ -13,4 +15,14 @@ def create_pg_route():
 @process_group_bp.route("/", methods=["GET"])
 def get_pg_route():
     result = get_process_group_by_id(client)
+    return jsonify(result)
+
+@process_group_bp.route("/update-name", methods=["PUT"])
+def update_pg_name_route():
+    data = request.get_json()
+    new_name = data.get("new_name")
+    if not new_name:
+        return jsonify({"error": "new_name is required"}), 400
+
+    result = update_process_group_name(client, new_name)
     return jsonify(result)
